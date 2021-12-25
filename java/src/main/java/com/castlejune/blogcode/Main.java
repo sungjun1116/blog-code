69cbe17f6c4a37dc6c885f73f6b144b89151f126
황성준 <opr99@naver.com>
2021-12-26 00:54:07 +0900

    volatile 필드를 사용해 스레드가 정상 종료한다.

--- java/src/main/java/com/castlejune/blogcode/Main.java
+++ java/src/main/java/com/castlejune/blogcode/Main.java
@@ -3,20 +3,12 @@ package com.castlejune.blogcode;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static boolean stopRequested;
-
-    private static synchronized void requestStop() {
-        stopRequested = true;
-    }
-
-    private static synchronized boolean stopRequested() {
-        return stopRequested;
-    }
+    private static volatile boolean stopRequested;
 
     public static void main(String[] args) throws InterruptedException {
         Thread backgroundThread = new Thread(() -> {
             int i = 0;
-            while (!stopRequested()) {
+            while (!stopRequested) {
                 i++;
             }
 
@@ -24,6 +16,6 @@ public class Main {
         backgroundThread.start();
 
         TimeUnit.SECONDS.sleep(1);
-        requestStop();
+        stopRequested = true;
     }
 }
