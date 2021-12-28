4a88bba28a743c86676993bb76ede7b4d7d40ba5
황성준 <opr99@naver.com>
2021-12-28 23:25:46 +0900

    [lambda] 람다를 인스턴스 필드에 저장해 상수별 동작을 구현한 열거 타입

--- java/src/main/java/com/castlejune/blogcode/enums/Operation.java
+++ java/src/main/java/com/castlejune/blogcode/enums/Operation.java
@@ -1,25 +1,25 @@
 package com.castlejune.blogcode.enums;
 
+import java.util.function.DoubleBinaryOperator;
+
 public enum Operation {
-    PLUS("+") {
-        public double apply(double x, double y) { return x + y; }
-    },
-    MINUS("-") {
-        public double apply(double x, double y) { return x - y; }
-    },
-    TIMES("*") {
-        public double apply(double x, double y) { return x * y; }
-    },
-    DIVIDE("/") {
-        public double apply(double x, double y) { return x / y; }
-    };
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
 
     private final String symbol;
+    private final DoubleBinaryOperator op;
 
-    Operation(String symbol) { this.symbol = symbol; }
+    Operation(String symbol, DoubleBinaryOperator op) {
+        this.symbol = symbol;
+        this.op = op;
+    }
 
     @Override
     public String toString() { return symbol; }
 
-    public abstract double apply(double x, double y);
+    public double apply(double x, double y) {
+        return op.applyAsDouble(x, y);
+    }
 }
