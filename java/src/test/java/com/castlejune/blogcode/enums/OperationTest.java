5c82c581bb9e6604a64695c6b2e3aa10a95bc546
황성준 <opr99@naver.com>
2021-12-29 01:10:54 +0900

    [lambda] OperationTest 코드 작성

---
+++ java/src/test/java/com/castlejune/blogcode/enums/OperationTest.java
@@ -0,0 +1,25 @@
+package com.castlejune.blogcode.enums;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OperationTest {
+
+    @Test
+    @DisplayName("람다 테스트")
+    void test1() {
+        // given
+        double a = 20;
+        double b = 10;
+
+        // when && then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(Operation.PLUS.apply(a, b)).isEqualTo(30);
+            softAssertions.assertThat(Operation.MINUS.apply(a, b)).isEqualTo(10);
+            softAssertions.assertThat(Operation.TIMES.apply(a, b)).isEqualTo(200);
+            softAssertions.assertThat(Operation.DIVIDE.apply(a, b)).isEqualTo(2);
+        });
+    }
+}
