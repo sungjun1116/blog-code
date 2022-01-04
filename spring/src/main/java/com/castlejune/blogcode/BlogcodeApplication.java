b1dece42c009959576d1d1a3697e37577070f886
황성준 <opr99@naver.com>
2022-01-04 23:55:56 +0900

    Initial commit

---
+++ spring/src/main/java/com/castlejune/blogcode/BlogcodeApplication.java
@@ -0,0 +1,13 @@
+package com.castlejune.blogcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcodeApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/castlejune/blogcode/BlogcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castlejune.blogcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
